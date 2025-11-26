d3f3407c3fb7db1414592991271744e4ce8a6ab3
Hüseyin Şimşek <Huseyin.Simsek@obase.com>
2025-11-26 15:30:14 +0300

    sqlite support

--- Data/TodoItem.cs
+++ Data/TodoItem.cs
@@ -1,7 +1,11 @@
-﻿namespace CetTodoApp.Data;
+﻿using SQLite;
 
+namespace CetTodoApp.Data;
+
+[Table("TodoItems")]
 public class TodoItem
 {
+    [PrimaryKey, AutoIncrement]
     public int Id {get; set;}
     public string? Title {get; set;}
     public DateTime DueDate { get; set; }
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,14 +4,14 @@ namespace CetTodoApp;
 
 public partial class MainPage : ContentPage
 {
-   
+    private ToDoDB db=new ToDoDB();
 
     public MainPage()
     {
         InitializeComponent();
-        FakeDb.AddToDo("Test1" ,DateTime.Now.AddDays(-1));
-        FakeDb.AddToDo("Test2" ,DateTime.Now.AddDays(1));
-        FakeDb.AddToDo("Test3" ,DateTime.Now);
+        //FakeDb.AddToDo("Test1" ,DateTime.Now.AddDays(-1));
+        //FakeDb.AddToDo("Test2" ,DateTime.Now.AddDays(1));
+        //FakeDb.AddToDo("Test3" ,DateTime.Now);
         RefreshListView();
         ;
 
@@ -19,27 +19,30 @@ public partial class MainPage : ContentPage
     }
 
 
-    private void AddButton_OnClicked(object? sender, EventArgs e)
+    private async void AddButton_OnClicked(object? sender, EventArgs e)
     {
-        FakeDb.AddToDo(Title.Text, DueDate.Date);
+        //FakeDb.AddToDo(Title.Text, DueDate.Date);
+        await db.CreateAsync(Title.Text, DueDate.Date);
         Title.Text = string.Empty;
         DueDate.Date=DateTime.Now;
-        RefreshListView();
+        await RefreshListView();
     }
 
-    private void RefreshListView()
+    private async Task RefreshListView()
     {
         TasksListView.ItemsSource = null;
-        TasksListView.ItemsSource = FakeDb.Data.Where(x => !x.IsComplete ||
-                                                           (x.IsComplete && x.DueDate > DateTime.Now.AddDays(-1)))
-            .ToList();
+        TasksListView.ItemsSource = await db.GetAllAsync();
+        //. FakeDb.Data.Where(x => !x.IsComplete ||
+        //                                             (x.IsComplete && x.DueDate > DateTime.Now.AddDays(-1)))
+        //.ToList();
     }
 
-    private void TasksListView_OnItemSelected(object? sender, SelectedItemChangedEventArgs e)
+    private async void TasksListView_OnItemSelected(object? sender, SelectedItemChangedEventArgs e)
     {
         var item = e.SelectedItem as TodoItem;
-       FakeDb.ChageCompletionStatus(item);
-       RefreshListView();
+        await db.TogleCompletionStatusAync(item!);
+      // FakeDb.ChageCompletionStatus(item);
+       await RefreshListView();
        
     }
 }
