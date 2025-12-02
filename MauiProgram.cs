873ef997e37842eba1a06a49b81421645e62f2fa
ilginyuksel0-max <ilginyuksel0@gmail.com>
2025-12-02 11:37:49 +0300

    Update MauiProgram.cs

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,5 +1,35 @@
-﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging;
 
+namespace CetTodoApp;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			// ... diğer ayarlar
+
+            // *** Burası ÖNEMLİ: Veritabanı Servisini DI'ya ekleme ***
+            .Services.AddSingleton<TodoDatabase>(s =>
+            {
+                // Veritabanı dosya adını belirle
+                var dbName = "TodoSQLite.db3"; 
+                // Platforma özel kalıcı uygulama veri dizinini al
+                string dbPath = Path.Combine(FileSystem.AppDataDirectory, dbName); 
+                
+                // TodoDatabase örneğini oluştur ve DI'ya kaydet
+                return new TodoDatabase(dbPath);
+            });
+            // --------------------------------------------------------
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
 namespace CetTodoApp;
 
 public static class MauiProgram
@@ -20,5 +50,5 @@ public static class MauiProgram
 #endif
 
         return builder.Build();
-    }
+    
 }
